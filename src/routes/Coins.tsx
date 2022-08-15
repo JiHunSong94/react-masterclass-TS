38d2c80f419cf9aa6fa81e42c6329a21af97260a
Jihun Song <songjihun940409@gmail.com>
2022-08-16 01:16:03 +0900

    3.10 CRYPTO TRACKER - React Query part Two

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { fetchCoins } from "./api";
