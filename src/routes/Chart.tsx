af29f58d5f11ca9c8683c70c425b6c6b6961de2b
Jihun Song <songjihun940409@gmail.com>
2022-08-17 01:04:19 +0900

    3.12 CRYPTO TRACKER - Price Chart part Two

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,15 +1,62 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchCoinHistory } from "./api";
+import ApexChart from "react-apexcharts";
 
 interface ChartProps {
   coinId: string;
 }
 
+interface IHistorical {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
+}
+
 function Chart({ coinId }: ChartProps) {
-  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId)
   );
-  return <h1>Chart</h1>;
+
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexChart
+          options={{
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: { show: false },
+              background: "transparent",
+            },
+            theme: { mode: "dark" },
+            stroke: { curve: "smooth", width: 4 },
+            grid: {
+              show: false,
+            },
+            yaxis: { show: false },
+            xaxis: {
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+              labels: { show: false },
+            },
+          }}
+          series={[
+            {
+              name: "price",
+              data: data?.map((price) => parseFloat(price.close)) as number[],
+            },
+          ]}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
